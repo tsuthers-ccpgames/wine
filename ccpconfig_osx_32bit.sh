0ae87450292817c073dd51dfb7adfeafc78019af
tim <tim@not-really-sure-why-you-need-an-email-git-usernames-work-fine-on-every-other-vcs>
2019-02-14 15:37:23 +0000

    adjusting paths to allow combined 64/32 build

--- ccpconfig_osx_32bit.sh
+++ ccpconfig_osx_32bit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # TODO: as per these instructions get the 32bit libs from somewhere: https://wiki.winehq.org/Building_Wine#Shared_WoW64
-./configure \
+../configure \
 	CC="clang" \
 	CXX="clang++" \
 	CFLAGS="-std=gnu89 -g -DWINE_NO_DEBUG_MSGS=1" \
 	LDFLAGS="-L/usr/local/opt/libxml2/lib" \
 	CPPFLAGS="-I/usr/local/opt/libxml2/include/libxml2" \
 	prefix=$1 \
-        --with-wine64=../wine64-build
+        --with-wine64=../build_x64
